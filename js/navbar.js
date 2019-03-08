25277d378089c7d9e3adf2b5d580ac9748e4774e
Adrian <wegrzyn.adr@gmail.com>
2019-03-08 16:06:53 +0100

    javascript

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+let nav = '<nav>' +
+    '<a href="../html/index.html">Home</a>' +
+    '<a  href="#">Blog</a>' +
+    '<a  href="#">Quiz</a>' +
+    '<a href="../html/contact.html">Kontakt</a>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
