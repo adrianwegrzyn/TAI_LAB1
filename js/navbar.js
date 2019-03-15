719695e1fa66b22aac30f62f1cd534e6140bb1da
Adrian <wegrzyn.adr@gmail.com>
2019-03-15 16:00:42 +0100

    add "up button"

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 let nav = '<nav>' +
-    '<a href="../index.html">Home</a>' +
+    '<a href="index.html">Home</a>' +
     '<a  href="#">Blog</a>' +
     '<a  href="#">Quiz</a>' +
-    '<a href="../contact.html">Kontakt</a>';
+    '<a href="contact.html">Kontakt</a>';
 
 let elem = document.querySelector(".navbar");
 elem.innerHTML = nav;
