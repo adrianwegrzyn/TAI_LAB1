e0c23ffcb9075f3643ba57a71d7ccc5856e46245
Adrian <wegrzyn.adr@gmail.com>
2019-03-20 11:47:38 +0100

    ready task

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 let nav = '<nav>' +
     '<a href="index.html">Home</a>' +
     '<a  href="#">Blog</a>' +
-    '<a  href="#">Quiz</a>' +
+    '<a  href="quiz.html">Quiz</a>' +
     '<a href="contact.html">Kontakt</a>';
 
 let elem = document.querySelector(".navbar");
